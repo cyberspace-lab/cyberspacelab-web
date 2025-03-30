c8d7802d93a3d117d560b411b9f6168466a2a00c
Lukáš Hejtmánek <hejtmy@gmail.com>
2025-03-30 14:06:12 +0200

    fixes about page

---
+++ astro/src/components/BulletPointBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="inner-box">
+    <div v-for="(aim, i) in aims" :key="i" class="single-item">
+      <div class="count-box">
+        <span>0{{ i + 1 }}</span>
+      </div>
+      <div class="inner">
+        <h3>{{ aim[0] }}</h3>
+        <p>{{ aim[1] }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AimsBox',
+  props: {
+    aims: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
