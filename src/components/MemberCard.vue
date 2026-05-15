892c99445c24b0b95766b6071f206c5034186f63
Lukáš Hejtmánek <hejtmy@gmail.com>
2026-05-15 17:42:12 +0200

    Style updates

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -26,38 +26,40 @@ const memberInitials = initials(d.title);
 </script>
 
 <template>
-  <a :href="`/team/${d.slug}`" class="csl-member-card">
-    <div class="csl-member-portrait" :style="{ borderColor: accent }">
-      <img
-        v-if="d.featuredImage"
-        :src="d.featuredImage"
-        :alt="d.title"
-      />
-      <div
-        v-else
-        class="csl-member-initials"
-        :style="{ color: accent, textShadow: `0 0 18px ${accent}` }"
-      >{{ memberInitials }}</div>
-      <div class="csl-member-scan" />
-    </div>
-
-    <div class="csl-member-body">
-      <h3>{{ d.title }}</h3>
-      <div class="csl-member-level" :style="{ color: accent }">{{ d.level || d.role }}</div>
-
-      <div v-if="d.education && d.education.length" class="csl-member-tags">
-        <span
-          v-for="tag in d.education.slice(0, 2)"
-          :key="tag"
-          class="csl-tag"
-        >{{ tag }}</span>
+  <div class="csl-member-card">
+    <a :href="`/team/${d.slug}`" class="csl-member-card-inner">
+      <div class="csl-member-portrait" :style="{ borderColor: accent }">
+        <img
+          v-if="d.featuredImage"
+          :src="d.featuredImage"
+          :alt="d.title"
+        />
+        <div
+          v-else
+          class="csl-member-initials"
+          :style="{ color: accent, textShadow: `0 0 18px ${accent}` }"
+        >{{ memberInitials }}</div>
+        <div class="csl-member-scan" />
       </div>
 
-      <div v-if="d.social" class="csl-member-socials">
-        <a v-if="d.social.twitter"     class="csl-member-social-link" :href="d.social.twitter"     target="_blank" rel="noopener noreferrer" @click.stop>TW</a>
-        <a v-if="d.social.linkedin"    class="csl-member-social-link" :href="d.social.linkedin"    target="_blank" rel="noopener noreferrer" @click.stop>LI</a>
-        <a v-if="d.social.researchgate" class="csl-member-social-link" :href="d.social.researchgate" target="_blank" rel="noopener noreferrer" @click.stop>RG</a>
+      <div class="csl-member-body">
+        <h3>{{ d.title }}</h3>
+        <div class="csl-member-level" :style="{ color: accent }">{{ d.level || d.role }}</div>
+
+        <div v-if="d.education && d.education.length" class="csl-member-tags">
+          <span
+            v-for="tag in d.education.slice(0, 2)"
+            :key="tag"
+            class="csl-tag"
+          >{{ tag }}</span>
+        </div>
       </div>
+    </a>
+
+    <div v-if="d.social" class="csl-member-socials">
+      <a v-if="d.social.twitter"     class="csl-member-social-link" :href="d.social.twitter"     target="_blank" rel="noopener noreferrer">TW</a>
+      <a v-if="d.social.linkedin"    class="csl-member-social-link" :href="d.social.linkedin"    target="_blank" rel="noopener noreferrer">LI</a>
+      <a v-if="d.social.researchgate" class="csl-member-social-link" :href="d.social.researchgate" target="_blank" rel="noopener noreferrer">RG</a>
     </div>
-  </a>
+  </div>
 </template>
